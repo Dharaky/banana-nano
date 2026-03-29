605edc0fc3404e823d8d1c7670a96fe47bd7ccf8
Allen <allen@example.com>
2026-03-29 01:07:16 -0700

    Move car graphic even more to the left in the challenge header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -550,7 +550,7 @@ const Home = () => {
           {showPills && !isChallengeEnded && (
             <div className="flex flex-col items-center gap-2 flex-1 relative group">
               <div className="h-[48px] flex flex-col items-center justify-end w-full">
-                <div className="flex items-center gap-3">
+                <div className="flex items-center gap-1">
                   <img src="/pley-challenge-logo.png" alt="Challenge House" className="h-[40px] w-auto object-contain" />
                   <img src="/pley-challenge-car.png" alt="Challenge Car" className="h-[28px] w-auto object-contain" />
                 </div>
