23ef0fd6d98400f6fa8ed4971f61682b15b8f0d1
Allen <allen@example.com>
2026-03-29 01:20:49 -0700

    Pull the garage graphic slightly left using negative margin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -553,7 +553,7 @@ const Home = () => {
                 <div className="flex items-center gap-1">
                   <img src="/pley-challenge-logo.png" alt="Challenge House" className="h-[40px] w-auto object-contain" />
                   <img src="/pley-challenge-car.png" alt="Challenge Car" className="h-[28px] w-auto object-contain" />
-                  <img src="/pley-challenge-garage.png" alt="Challenge Garage" className="h-[36px] w-auto object-contain" />
+                  <img src="/pley-challenge-garage.png" alt="Challenge Garage" className="h-[36px] w-auto object-contain -ml-2" />
                 </div>
               </div>
             </div>
